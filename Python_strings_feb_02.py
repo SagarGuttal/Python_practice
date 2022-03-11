80274792a0535f3a526db0ef4c8b0c56bf9fbc13
Sagar guttal <90856447+Saguu1434@users.noreply.github.com>
2022-03-11 19:12:40 +0530

    Add files via upload

---
+++ Python_strings_feb_02.py
@@ -0,0 +1,60 @@
+##var="Sagar Guttal"
+##print(var)
+##
+####print(var[0:])
+####print(var[::])
+####print(var[-1:-12])
+####
+####print(var[::-1])
+##
+##var,var1="sagar","Guttal"
+##print("my first name"+" "+var)
+##print("last name is"+" "+var1)
+
+##name="Dhoni"
+##Score=183
+##statement="captain "+name+" scored "+str(Score)+" against Shrilanka"
+##print(statement)
+##
+##statement1="captain %s scored %d against Shrilanka"%(name,Score)
+##print(statement1)
+##
+##statement2="captain {} scored {} against Shrilanka".format(name,Score)
+##print(statement2)
+##
+##statement3=f"captain {name} scored {Score} against Shrilanka"
+##print(statement3)
+
+##x="dhoni is my captain  # error
+##he plays for CSK"
+##
+##x="""Dhoni is my captain
+##he plays for CSk"""
+##print(x)
+##
+##y= "Dhoni is my captain \
+##he plays for CSk "
+##print(y)
+##
+##z="c:\newfolder\cdfg\dfdg.text"
+##print(z)
+##
+##z=r"c:\newfolder\cdfg\dfdg.text"
+##print(z)
+
+first_name=input("Enter first name: " )
+age=int(input("Enter your age :"))
+
+print("your name is : ",first_name)
+print(type(first_name))
+print("your age is : ",age)
+print(type(age))
+
+
+
+
+
+
+
+
+
